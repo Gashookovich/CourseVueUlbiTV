94e56cc161548390ff14d58fcc7637df63461d44
Vadim <92977297+Gashookovich@users.noreply.github.com>
2022-07-06 20:34:37 +0300

    courseVue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <navbar-page></navbar-page>
+  <div class="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import NavbarPage from "@/components/NavbarPage";
+export default {
+  components: {NavbarPage}
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+}
+</style>
---
+++ src/components/NavbarPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="navbar">
+    <div>Vue 3</div>
+    <div class="navbar__btns">
+      <my-button @click="$router.push('/posts')">Посты</my-button>
+      <my-button @click="$router.push('/about')" style="margin-left: 20px">О сайте</my-button>
+      <my-button @click="$router.push('/store')" style="margin-left: 20px">Store</my-button>
+      <my-button @click="$router.push('/composition')" style="margin-left: 20px">Composition</my-button>
+      <my-button @click="$router.push('/')" style="margin-left: 20px">Главная</my-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.navbar {
+  height: 50px;
+  background-color: lightgray;
+  box-shadow: 2px 2px 4px gray;
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+}
+.navbar__btns {
+  margin-left: auto;
+}
+</style>
